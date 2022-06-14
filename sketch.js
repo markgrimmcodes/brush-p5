7e0b203516991478b3c90201981957e857a56593
Mark Grimm <mark.grimm.coding@gmail.com>
2022-06-14 14:10:13 +0200

    feat: adds iceBrush, eraserBrush and switch case

--- sketch.js
+++ sketch.js
@@ -1,13 +1,34 @@
+let bgColor = "white";
+let currentBrush = "";
+
 function setup() {
   createCanvas(400, 400);
-  background("white");
+  background(bgColor);
   noStroke();
 }
 function draw() {
+  switch (key) {
+    case "f":
+      currentBrush = "fire";
+      break;
+    case "i":
+      currentBrush = "ice";
+      break;
+    default:
+      currentBrush = "";
+  }
+
   if (mouseIsPressed) {
-    fireBrush(mouseX, mouseY);
+    if (currentBrush === "fire") {
+      fireBrush(mouseX, mouseY);
+    } else if (currentBrush === "ice") {
+      iceBrush(mouseX, mouseY);
+    } else {
+      eraserBrush(mouseX, mouseY);
+    }
   }
 }
+
 function fireBrush(x, y) {
   x = x + random(-20, 20);
   y = y + random(-20, 20);
@@ -25,4 +46,25 @@ function fireBrush(x, y) {
   fill(random(200, 255), green, 0, opacity);
 
   circle(x, y, random(10, 30));
+}
+function iceBrush(x, y) {
+  let opacity = random(0, 100);
+  x = x + random(-10, 10);
+  y = y + random(-10, 10);
+  let blue = random(200, 255);
+
+  let bigRectHeight = random(30, 40);
+  let bigRectWidth = random(30, 40);
+
+  let smallRectHeight = random(10, 20);
+  let smallRectWidth = random(10, 20);
+
+  rect(mouseX, mouseY, bigRectHeight, bigRectWidth);
+  rect(x, y, smallRectHeight, smallRectWidth);
+
+  fill(0, 0, blue, opacity);
+}
+function eraserBrush(x, y) {
+  fill(bgColor);
+  circle(x, y, 50);
 }
