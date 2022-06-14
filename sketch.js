cd1911fbd804a50aec145fc6a80e3b6ce6fc2745
Mark Grimm <mark.grimm.coding@gmail.com>
2022-06-14 09:51:59 +0200

    feat:adds fire brush effect on mouseclick

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
 function setup() {
   createCanvas(400, 400);
+  background("white");
+  noStroke();
 }
-
 function draw() {
-  background(220);
+  if (mouseIsPressed) {
+    let diameter = random(10, 30);
+    let green = 0;
+    let opacity = 100;
+    if (diameter < 20) {
+      opacity = random(10, 40);
+    } else {
+      green = random(100, 200);
+      opacity = random(40, 70);
+    }
+    fill(random(200, 255), green, 0, opacity);
+    let x = mouseX + random(-20, 20);
+    let y = mouseY + random(-20, 20);
+    circle(x, y, random(10, 30));
+  }
 }
