ef23dac27bbf3ac62a4cf5839fd43db7ef0fba84
Mark Grimm <mark.grimm.coding@gmail.com>
2022-06-14 10:48:01 +0200

    feat: refactoring funcion fireBrush

--- sketch.js
+++ sketch.js
@@ -5,18 +5,24 @@ function setup() {
 }
 function draw() {
   if (mouseIsPressed) {
-    let diameter = random(10, 30);
-    let green = 0;
-    let opacity = 100;
-    if (diameter < 20) {
-      opacity = random(10, 40);
-    } else {
-      green = random(100, 200);
-      opacity = random(40, 70);
-    }
-    fill(random(200, 255), green, 0, opacity);
-    let x = mouseX + random(-20, 20);
-    let y = mouseY + random(-20, 20);
-    circle(x, y, random(10, 30));
+    fireBrush(mouseX, mouseY);
   }
+}
+function fireBrush(x, y) {
+  x = x + random(-20, 20);
+  y = y + random(-20, 20);
+
+  let diameter = random(10, 30);
+  let green = 0;
+  let opacity = 100;
+
+  if (diameter < 20) {
+    opacity = random(10, 40);
+  } else {
+    green = random(100, 200);
+    opacity = random(40, 70);
+  }
+  fill(random(200, 255), green, 0, opacity);
+
+  circle(x, y, random(10, 30));
 }
